25f6acef31507e050404a949f7232e8acce30717
bhumika <62795443+bhumika0428@users.noreply.github.com>
2025-07-11 11:27:47 +0530

    Add files via upload

---
+++ Java–Spring(Module-6)/IntroductiontoStsApplication/src/main/java/com/top/demo/IntroductiontoStsApplication.java
@@ -0,0 +1,14 @@
+package com.top.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductiontoStsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(IntroductiontoStsApplication.class, args);
+	}
+
+}
